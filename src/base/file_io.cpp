fe0ce93cddb2c8993b95f01aff00e5ac7eb33abf
perjoh <perjoh@gmail.com>
2017-05-21 22:17:38 +0200

    file_io

---
+++ src/base/file_io.cpp
@@ -0,0 +1,24 @@
+#include "file_io.hpp"
+#include <fstream>
+
+namespace kvant {
+namespace base {
+
+    std::string read_file(const char* filename)
+    {
+        std::string result;
+
+        std::ifstream file(filename, std::ios::binary | std::ios::ate);
+        if (file.is_open()) 
+        {
+            const size_t filesize = static_cast<size_t>(file.tellg());
+            file.seekg(0, std::ios::beg);
+            result.resize(filesize);
+            file.read(&result[0], filesize);
+        }
+
+        return result;
+    }
+
+} // namespace base
+} // namespace kvant
