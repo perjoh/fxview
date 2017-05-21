595d6662d75dd6746fac2fbf68397d86a9130caa
perjoh <perjoh@gmail.com>
2017-05-21 22:17:54 +0200

    formatting

--- src/base/task_runner.cpp
+++ src/base/task_runner.cpp
@@ -1,39 +1,40 @@
 #include "task_runner.hpp"
-#include <algorithm>
-#include <SDL.h>
 
-namespace base
-{
-	Task_runner& Task_runner::instance()
-	{
-		static Task_runner inst;
-		return inst;
-	}
+namespace kvant {
+namespace base {
 
-	void Task_runner::add_task(Task_delegate f)
-	{
-		tasks_.push_back(f);
-	}
+    Task_runner& Task_runner::instance()
+    {
+        static Task_runner inst;
+        return inst;
+    }
 
-	void Task_runner::run()
-	{
-		// Iterate by index to avoid problem with iterator invalidation if new tasks are added.
-		for (current_task_ = 0; current_task_ < tasks_.size(); ++current_task_)
-		{
-			tasks_[current_task_](); 
-		}
-	}
+    void Task_runner::add_task(Task_delegate f)
+    {
+        tasks_.push_back(f);
+    }
 
-	void Task_runner::end_current()
-	{
-		tasks_[current_task_] = tasks_.back();
-		tasks_.pop_back(); 
-		--current_task_;
-	}
+    void Task_runner::run()
+    {
+        // Iterate by index to avoid problem with iterator invalidation if new tasks are added.
+        for (current_task_ = 0; current_task_ < tasks_.size(); ++current_task_)
+        {
+            tasks_[current_task_]();
+        }
+    }
 
-	Task_runner::Task_runner()
-		: current_task_(0)
-	{ 
-		tasks_.reserve(64); // Speculative.
-	}
-}
+    void Task_runner::end_current()
+    {
+        tasks_[current_task_] = tasks_.back();
+        tasks_.pop_back();
+        --current_task_;
+    }
+
+    Task_runner::Task_runner()
+        : current_task_(0)
+    {
+        tasks_.reserve(64); // Speculative.
+    }
+
+} // namespace base
+} // namespace kvant
--- src/base/task_runner.hpp
+++ src/base/task_runner.hpp
@@ -2,25 +2,27 @@
 #include <vector>
 #include "fast_delegate.hpp"
 
-namespace base
-{ 
-	class Task_runner
-	{
-	public :
-		static Task_runner& instance();
+namespace kvant {
+namespace base {
 
-	public : 
-		using Task_delegate = Fast_delegate<void, void>;
-		void add_task(Task_delegate f); 
+    class Task_runner {
+    public:
+        static Task_runner& instance();
 
-	public :
-		void run();
+    public:
+        using Task_delegate = Fast_delegate<void>;
+        void add_task(Task_delegate f);
 
-		void end_current();
+    public:
+        void run();
 
-	private :
-		Task_runner();
-		std::vector<Task_delegate> tasks_;
-		size_t current_task_;
-	};
-}
+        void end_current();
+
+    private:
+        Task_runner();
+        std::vector<Task_delegate> tasks_;
+        size_t current_task_;
+    };
+
+} // namespace base
+} // namespace kvant
