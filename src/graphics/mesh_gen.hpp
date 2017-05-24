8ccb1f3f9f759412197f51fc41ad7c2df09309d7
perjoh <perjoh@gmail.com>
2017-05-24 10:01:41 +0200

    mesh_gen

--- src/graphics/mesh_gen.hpp
+++ src/graphics/mesh_gen.hpp
@@ -7,7 +7,7 @@ namespace mesh {
     mesh::Triangle_mesh<> generate_smooth_cube(const glm::vec3& size, unsigned level_of_detail = 4);
     mesh::Triangle_mesh<> generate_cube(const glm::vec3& size);
 
-    mesh::Triangle_mesh<> generate_grid(unsigned int width, unsigned int height, float unit_size = 1.0f);
+    mesh::Triangle_mesh<> generate_checkerboard(unsigned int width, unsigned int height, float unit_size = 1.0f);
 
 } // namespace shape_gen
 } // namespace graphics 
